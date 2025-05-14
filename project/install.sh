4249b12e46c1f079c05caeb6ceef29e15aba3061
Leon <132808925+Leon8326@users.noreply.github.com>
2025-05-14 14:34:18 +0200

    Update install.sh

--- project/install.sh
+++ project/install.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 
 sudo git clone https://github.com/NarpLang/bitey.git /usr/bitey
-echo "Add this line to ~/.bashrc: export PATH=PATH:/usr/bitey/bin"
+echo "Add this line to ~/.bashrc: export PATH=$PATH:/usr/bitey/bin"
 echo "Now reload your shell or run 'source ~/.bashrc' to use Bitey!"
