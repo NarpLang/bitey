7848f303f42b7a8c1f0e957e9141ffe300d3337a
Leon <132808925+Leon8326@users.noreply.github.com>
2025-05-21 16:31:18 +0200

    Update install.sh

--- project/install.sh
+++ project/install.sh
@@ -1,6 +1,9 @@
 #!/bin/bash
 
 sudo git clone https://github.com/NarpLang/bitey.git /usr/bitey
+sudo mkdir /usr/bitey/software
+sudo git clone https://github.com/NarpIndex/remote-main.git /usr/bitey/software/remote-main
+sudo git clone https://github.com/NarpIndex/remote-dev.git /usr/bitey/software/remote-dev
 sudo chmod +x /usr/bitey/bin/*
 sudo ln -sf /usr/bitey/bin/bitey /usr/bin/bitey
 echo "🎉 Bitey is now installed!"
