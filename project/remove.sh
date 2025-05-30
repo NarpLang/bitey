3d8af9b3adec3fb19020306a7996ad5430711ac3
Leon <132808925+Leon8326@users.noreply.github.com>
2025-05-30 16:11:25 +0200

    Update remove.sh

--- project/remove.sh
+++ project/remove.sh
@@ -1,10 +1,11 @@
 #!/bin/bash
 
-echo -e "🐶 Hey you! Yes, you! Are you sure you want to remove Bitey? All of your installled packages will also be removed. \n"
-printf "Please Pick! [y/N] "
+echo -e "🐶 Hey you! Yes, you! Are you sure you want to remove Bitey? All of your installed packages will also be removed.\n"
+printf "Please pick [y/N]: "
 read choice
-if [[ $choice == "y" ]]; then
-    echo "🐶 Goodbye $(whoami)!!!"
+
+if [[ "${choice,,}" == "y" ]]; then
+    echo "🐶 Goodbye you, and $(hostname) too!!!"
     exit 0
 else
     echo "🐶 Removal cancelled."
